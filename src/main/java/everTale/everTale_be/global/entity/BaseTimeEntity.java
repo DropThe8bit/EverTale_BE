ce3e9cf723e3f672e965094ad43ab19e09c452b5
hhongyeahh <hhongyeahh@gmail.com>
2025-06-23 21:23:21 +0900

    [feat] BaseTimeEntity 전역으로 생성 #3

---
+++ src/main/java/everTale/everTale_be/global/entity/BaseTimeEntity.java
@@ -0,0 +1,25 @@
+package everTale.everTale_be.global.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(name="created_at", updatable = false, columnDefinition = "timestamp")
+    private LocalDateTime createdAt;
+
+    @LastModifiedBy
+    @Column(name="updated_at",columnDefinition = "timestmap")
+    private LocalDateTime updatedAt;
+}
