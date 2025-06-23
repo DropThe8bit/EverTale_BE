a01d66d1652368c23b7d507c208c3deb1e003dba
hhongyeahh <hhongyeahh@gmail.com>
2025-06-23 21:21:32 +0900

    [feat] story, scene entity 생성 #3

---
+++ src/main/java/everTale/everTale_be/domain/story/entity/Scene.java
@@ -0,0 +1,35 @@
+package everTale.everTale_be.domain.story.entity;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import everTale.everTale_be.domain.quiz.entity.Quiz;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Scene {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    private int page;
+
+    private String image_url;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "story_id", nullable = false)
+    private Story story;
+
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "quiz_id")
+    @JsonManagedReference
+    private Quiz quiz;
+
+
+}
+
