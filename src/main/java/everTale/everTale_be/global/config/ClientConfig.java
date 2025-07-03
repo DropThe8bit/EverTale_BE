1e7f7f4c6166e96e17f46761fd2059d9f948040e
hhongyeahh <hhongyeahh@gmail.com>
2025-07-03 14:15:25 +0900

    [chore] story 이미지 생성 및 파일 업로드 관련 Config
    - S3 관련
    - ClientConfig
    - MultipartInputStreamFileResource
    - OctetStreamReadMsgConverter

---
+++ src/main/java/everTale/everTale_be/global/config/ClientConfig.java
@@ -0,0 +1,13 @@
+package everTale.everTale_be.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ClientConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
