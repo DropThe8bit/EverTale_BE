18ae09f5b5921da5982d64204fe515529b6768a2
hhongyeahh <hhongyeahh@gmail.com>
2025-07-10 23:41:45 +0900

    [refactor] profileId 추가 및 EntityGraph 적용으로 N+1 문제 해결 #17
    - repository 메서드에 profileId 파라미터 추가
    - JPQL @Query 메서드 제거
    - EntityGraph로 연관 엔티티를 즉시 로딩하여 N+1 문제 방지

--- src/main/java/everTale/everTale_be/domain/story/repository/StoryRepository.java
+++ src/main/java/everTale/everTale_be/domain/story/repository/StoryRepository.java
@@ -1,7 +1,17 @@
 package everTale.everTale_be.domain.story.repository;
 
 import everTale.everTale_be.domain.story.entity.Story;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StoryRepository extends JpaRepository<Story, Long> {
+    @EntityGraph(attributePaths = {
+            "character",
+            "character.characterPersonalities",
+            "character.characterPersonalities.personality"
+    })
+    Optional<Story> findByIdAndProfileId(Long storyId, Long profileId);
+
 }
