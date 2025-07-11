e3ee325d5f12315b704fa552deb45f39883815b3
hhongyeahh <hhongyeahh@gmail.com>
2025-07-11 22:21:25 +0900

    [feat] Quiz 관련 ErrorStatus 정의 #11

--- src/main/java/everTale/everTale_be/global/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/everTale/everTale_be/global/apiPayload/code/status/ErrorStatus.java
@@ -52,7 +52,13 @@ public enum ErrorStatus implements BaseErrorCode {
 
 
     // EasterEgg 관련
-    EASTER_EGG_LETTER_NOT_FOUND(HttpStatus.NOT_FOUND,"EASTER_EGG400","이스터에그 편지를 찾을 수 없습니다.")
+    EASTER_EGG_LETTER_NOT_FOUND(HttpStatus.NOT_FOUND,"EASTER_EGG400","이스터에그 편지를 찾을 수 없습니다."),
+
+    //Quiz 관련
+    QUIZ_NOT_FOUND(HttpStatus.NOT_FOUND,"QUIZ400","퀴즈를 찾을 수 없습니다."),
+    ENABLE_TO_GENERATE_QUIZ(HttpStatus.BAD_REQUEST,"FASTAPI505","퀴즈 생성 중 오류가 발생했습니다."),
+    INVALID_QUIZ_ANSWER_NUM(HttpStatus.BAD_REQUEST,"QUIZ401","퀴즈 정답으로 선택할 수 없는 값입니다.")
+
 
     ;
 
