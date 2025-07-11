02b0e714133f40e2b286d55cbdff8087b8944627
hhongyeahh <hhongyeahh@gmail.com>
2025-07-11 22:09:15 +0900

    [feat] story, EasterEggLetter BaseTimeEntity 상속

--- src/main/java/everTale/everTale_be/domain/story/entity/Story.java
+++ src/main/java/everTale/everTale_be/domain/story/entity/Story.java
@@ -5,6 +5,7 @@ import everTale.everTale_be.domain.character.entity.StoryCharacter;
 import everTale.everTale_be.domain.easterEggLetter.entity.EasterEggLetter;
 import everTale.everTale_be.domain.story.entity.enums.Genre;
 import everTale.everTale_be.domain.profile.domain.Profile;
+import everTale.everTale_be.global.entity.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -16,7 +17,7 @@ import java.util.List;
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class Story {
+public class Story extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
