5cf94046e20cec690ecfae7e4788b50e698362bc
s2eojeong <155219307+s2eojeong@users.noreply.github.com>
2025-08-14 15:43:03 +0900

    fix: JsonProperty로 필드 바인딩 오류 해결

--- src/main/java/everTale/everTale_be/domain/easterEgg/dto/easterEggVoice/request/EasterEggVoiceRequestDto.java
+++ src/main/java/everTale/everTale_be/domain/easterEgg/dto/easterEggVoice/request/EasterEggVoiceRequestDto.java
@@ -1,5 +1,6 @@
 package everTale.everTale_be.domain.easterEgg.dto.easterEggVoice.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,9 +9,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class EasterEggVoiceRequestDto {
 
+    @JsonProperty("xCoordinate")
     @Schema(description = "사용자가 클릭한 이미지의 X 좌표", example = "135.0")
     private float xCoordinate;
 
+    @JsonProperty("yCoordinate")
     @Schema(description = "사용자가 클릭한 이미지의 Y 좌표", example = "210.0")
     private float yCoordinate;
 }
